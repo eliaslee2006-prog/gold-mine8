b0e4d6bce05d06bc2ff88378001077641e5e7e32
eliaslee2006-prog <eliaslee2006@gmail.com>
2026-09-09 14:10:58 +0800

    Add files via upload

--- js/api.js
+++ js/api.js
@@ -1,1 +1,10 @@
-
+export const API_BASE='https://api.eliaslhx.com';
+export async function api(path, options={}){
+  const init={credentials:'include',headers:{...(options.body?{'Content-Type':'application/json'}:{}),...(options.headers||{})},...options};
+  if(options.body && typeof options.body!=='string') init.body=JSON.stringify(options.body);
+  const res=await fetch(`${API_BASE}${path}`,init);
+  let data=null;try{data=await res.json();}catch{}
+  if(!res.ok){const err=new Error(data?.error||`HTTP ${res.status}`);err.status=res.status;err.data=data;throw err;}
+  return data;
+}
+export const connectGoogle=()=>location.assign(`${API_BASE}/auth/google?returnTo=${encodeURIComponent(location.href.split('?')[0])}`);
