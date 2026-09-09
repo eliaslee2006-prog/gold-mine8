ae9ca9d78f1b90a8c0a92f58b3e56d9d3b149f16
eliaslee2006-prog <eliaslee2006@gmail.com>
2026-09-09 14:08:08 +0800

    Delete js/state.js

--- js/state.js
+++
@@ -1,3 +0,0 @@
-export const state={user:null,events:[],tasks:[],integrations:{},taskFilter:'active'};
-export function upsert(list,item){const i=list.findIndex(x=>x.id===item.id);if(i>=0)list[i]=item;else list.push(item);return item;}
-export function remove(list,id){const i=list.findIndex(x=>x.id===id);if(i>=0)list.splice(i,1);}
